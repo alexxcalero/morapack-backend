e31a4d5760443f8b41a310e6c41d61047c413db5
Alex Calero Revilla <alexcalerorevilla@gmail.com>
2025-10-25 12:12:37 -0500

    Eliminación de archivos

--- MoraPack/src/main/java/pe/edu/pucp/morapack/repositories/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.pucp.morapack.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.pucp.morapack.models.Producto;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-}
